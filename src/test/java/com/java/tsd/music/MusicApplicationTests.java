abfdb22f9e5fcba324765db80ccc6e00bd097553
sleepallday979 <1637005665@qq.com>
2022-04-05 11:45:36 +0800

    Initial commit

---
+++ src/test/java/com/java/tsd/music/MusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.tsd.music;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
